d232c3b0546299d4d5026103c340e53e325f66b9
caias <caias@wemakeprice.com>
2019-10-21 14:49:37 +0900

    merged pull request API test

--- src/popup/components/List.js
+++ src/popup/components/List.js
@@ -1,25 +1,28 @@
 'use strict';
 
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 
-const Badge = (isVisible) => {
-  console.log(typeof isVisible)
-  if (isVisible) {
-    return (
-      <span className="badge"></span>
-    )
-  }
+const Badge = () => {
+  return (
+    <span className="badge"></span>
+  );
+  
 }
 
-const Reviewer = ({ status }) => {
-  const { reviewers } = status;
+const Reviewer = ({props}) => {
+  const { reviewers } = props;
+  console.log(reviewers)
   return (
-    reviewers.map((member, index) => (
-      <span key={index} className={member.approved === true ? 'user approved' : 'user unapproved'}>
-        <img src={`http://stash.wemakeprice.com/users/${member.user.name}/avatar.png`} alt={member.user.displayName} original-title={member.user.displayName} />
-        <Badge isVisible={member.approved === true} />
-      </span>
-    ))
+    <div className="reviewers">
+      {reviewers.map((member, index) => (
+        <span key={index} className="user">
+          <img src={`http://stash.wemakeprice.com/users/${member.user.name}/avatar.png`} alt={member.user.displayName} title={member.user.displayName} />
+          {
+            member.approved && <Badge />
+          }
+        </span>
+      ))}
+    </div>
   )
 }
 
@@ -31,7 +34,11 @@ const List = ({ data }) => {
       {values.map((value, index) => (
         <li key={index}>
           <a href={`"https://stash.wemakeprice.com/projects/WONDER/repos/prerender/pull-requests/${value.id}`}>
-            <div className="user"><img src={`http://stash.wemakeprice.com/users/${value.author.user.name}/avatar.png`} alt={value.author.user.displayName} original-title={value.author.user.displayName}/></div>
+            <div className="pullrequest-author">
+              <span className="user">
+                <img src={`http://stash.wemakeprice.com/users/${value.author.user.name}/avatar.png`} alt={value.author.user.displayName} original-title={value.author.user.displayName}/>
+              </span>
+            </div>
             <div className="pullrequest-info">
               <div>
                 <span className="from">{value.fromRef.displayId}</span>
@@ -41,9 +48,7 @@ const List = ({ data }) => {
               <span className="displayName">{value.author.user.displayName}</span>
               <p>{value.title}</p>
             </div>
-            <div className="reviewers">
-              <Reviewer status={value} />
-            </div>
+            <Reviewer props={value} />
           </a>
         </li>
       ))}
